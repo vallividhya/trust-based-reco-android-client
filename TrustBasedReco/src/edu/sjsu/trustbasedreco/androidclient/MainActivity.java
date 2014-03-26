34e493f434c5066da9cb39992a71d5ffe4057850
vallividhya <vallividhyavenkatesan@gmail.com>
2014-03-26 08:32:22 -0700

    Sign up, sign in activites

---
+++ TrustBasedReco/src/edu/sjsu/trustbasedreco/androidclient/MainActivity.java
@@ -0,0 +1,82 @@
+package edu.sjsu.trustbasedreco.androidclient;
+
+import edu.sjsu.trustbasedreco.R;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    /* 
+     * Sign in user - checks session and navigates to bookmarks activity.
+     * */
+    public void signInUser(View view) {
+    	Log.d(ACTIVITY_SERVICE, "Bookmarks activity loading");
+    	Intent intent = new Intent(this, ShowBookmarksActivity.class);
+    	startActivity(intent);
+    }
+    
+    public void loadSignUpActivity(View view) {
+    	Log.d(ACTIVITY_SERVICE, "Sign Up activity loading");
+    	Intent intent = new Intent(this, SignUpActivity.class);
+    	startActivity(intent);
+    }
+    
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
---
+++ TrustBasedReco/src/edu/sjsu/trustbasedreco/androidclient/SignUpActivity.java
@@ -0,0 +1,75 @@
+package edu.sjsu.trustbasedreco.androidclient;
+
+import edu.sjsu.trustbasedreco.R;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class SignUpActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sign_up);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.sign_up, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void signUpUser(View view) {
+		// Make REST Call and save user data; 
+		// Ask user to invite friends, bookmark places
+		Log.d(ACTIVITY_SERVICE, "User signed up. Bookmarks activity loading");
+		Intent intent = new Intent(this, ShowBookmarksActivity.class);
+    	startActivity(intent);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_sign_up,
+					container, false);
+			return rootView;
+		}
+	}
+
+}
